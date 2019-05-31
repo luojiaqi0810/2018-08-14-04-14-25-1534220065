16831ecce74ef567784f11133518b1e226415314
birdegg <313993678@qq.com>
2019-05-31 15:00:49 +0800

    answer

--- main/main.js
+++ main/main.js
@@ -1,4 +1,59 @@
-module.exports = function main() {
+module.exports = function main(a) {
     console.log("Debug Info");
-    return 'Hello World!';
+    var firstLine = [
+	            '._.',
+	            '...',
+	            '._.',
+	            '._.',
+	            '...',
+	            '._.',
+	            '._.',
+	            '._.',
+	            '._.',
+	            '._.'
+	        ];
+	    var secondLine = [
+		            '|.|',
+		            '..|',
+		            '._|',
+		            '._|',
+		            '|_|',
+		            '|_.',
+		            '|_.',
+		            '..|',
+		            '|_|',
+		            '|_|'
+		        ];
+	    var thirdLine = [
+		            '|_|',
+		            '..|',
+		            '|_.',
+		            '._|',
+		            '..|',
+		            '._|',
+		            '|_|',
+		            '..|',
+		            '|_|',
+		            '..|'
+		        ];
+	    let result = '';
+	    for (var i = 0; i < a.length; i++) {
+		            result += firstLine[parseInt(a[i])];
+		    if(i!=a.length-1){
+		            result += " ";}
+		        }
+	    result += "\n";
+	    for (var i = 0; i < a.length; i++) {
+		            result += secondLine[parseInt(a[i])];
+if(i!=a.length-1){
+		            result += " ";}
+		        }
+	    result += "\n";
+	    for (var i = 0; i < a.length; i++) {
+		            result += thirdLine[parseInt(a[i])];
+if(i!=a.length-1){
+		            result += " ";}
+		        }
+	    result += "\n";
+	    return result;
 };
